d235fb9d0880a926489b81cb8c8241362acea18f
jaidevjoshi83 <jaidev53ster@gmail.com>
2020-06-15 02:55:20 -0500

    small bug fixed in forms.py

--- PeptideDB/DataBase/forms.py
+++ PeptideDB/DataBase/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class Rand_Frag_From(forms.Form):
 
     Sequence = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Ex.: AAAAAAGAAGGR'}),required=False)
-    Accession = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ex.: NA or Q86U42'}), required=False)
+    Accession = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ex.: Q86U42'}), required=False)
     #Frag_legnth = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ex.: Y,T,L,K'}))
  
 class Meta:
