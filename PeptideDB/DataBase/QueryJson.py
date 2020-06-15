fdcf27f403d3c800b4ee41f1d4d074c071b4628a
jaidevjoshi83 <jaidev53ster@gmail.com>
2020-06-15 00:36:56 -0500

    peptide and accession query added

--- PeptideDB/DataBase/QueryJson.py
+++ PeptideDB/DataBase/QueryJson.py
@@ -63,6 +63,28 @@ def QurJson(a=None,p=None):
       df = pd.DataFrame(list(zip(seq,starts_sides,end_sides,lenght)),columns=clms)
       df.to_json(Out_json_data, orient='split')
 
+   else:
+      Ps = PeptideSeq.objects.filter(Input_Sequence=p, Accession=a)
+
+      for C in Ps:
+
+         seq.append(C.Input_Sequence)
+         starts_sides.append(C.P1_Position)
+         end_sides.append(int(C.P1_Position)+(len(C.Input_Sequence)-1))
+         lenght.append(len(C.Input_Sequence))
+
+      clms = ['seq',           
+        'starts_sides', 
+        'end_sides', 
+        'lenght',]
+
+      BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+      Out_json_data = os.path.join(BASE_DIR,'DataBase','static','data','data.json')
+
+      df = pd.DataFrame(list(zip(seq,starts_sides,end_sides,lenght)),columns=clms)
+      df.to_json(Out_json_data, orient='split')
+
+
 
 
 
