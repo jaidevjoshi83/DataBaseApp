f505eb4c3cb9ab2d1711ea49d70c36b0e71ee77d
Joshi <joshij@lri-108042.lerner.ad.cchs.net>
2022-11-17 10:25:13 -0500

    home page fixed

--- PeptideDB/DataBase/forms.py
+++ PeptideDB/DataBase/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class Rand_Frag_From(forms.Form):
 
-    Sequence = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter protein ID; Ex. alpha '}),required=False)
-    Accession = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Description; Ex. P02671 '}), required=False)
+    Sequence = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Protein name; Ex. Tropomyosin beta chain '}),required=False)
+    Accession = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Uniprot ID; Ex. P02671 '}), required=False)
     #Frag_legnth = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ex.: Y,T,L,K'}))
  
 class Meta:
